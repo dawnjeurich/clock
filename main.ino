d0695e50a447a0947a6bdd7e9acd9497ae5255a1
James Halliday <mail@substack.net>
2015-09-18 20:18:28 -0700

    counting up!

--- main.ino
+++ main.ino
@@ -3,12 +3,22 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+unsigned long time;
+char stime[9];
 
 void setup() {                
   lcd.begin(16,2);
   lcd.setCursor(0,0);
-  lcd.print("BEEP BOOP");
 }
 
 void loop() {
+  time = millis();
+  byte seconds = time / 1000 % 60;
+  byte minutes = time / 1000 / 60 % 60;
+  byte hours = time / 1000 / 60 / 60 % 24;
+  char str[9];
+  sprintf(str, "%02d:%02d:%02d", hours, minutes, seconds);
+  lcd.setCursor(0,0);
+  lcd.print(str);
+  delay(250);
 }
